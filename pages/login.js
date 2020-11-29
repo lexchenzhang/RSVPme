1d8533296e7959e01aad90794212e42927f0c510
Lex <Lexzh@outlook.com>
2020-11-29 17:53:40 -0600

    format the code without changing it.

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Text, Button, AsyncStorage, TextInput } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Button,
+  AsyncStorage,
+  TextInput,
+} from "react-native";
 import { globalStyles } from "../styles/global";
 import * as AppAuth from "expo-app-auth";
-import * as Application from 'expo-application'
+import * as Application from "expo-application";
 import axios from "axios";
 import { set } from "react-native-reanimated";
 const qs = require("qs");
@@ -13,7 +20,7 @@ export default function Login() {
   let [creatingAccount, setCreatingAccount] = useState(false);
   let [username, setUsername] = useState("");
   let [loading, setLoading] = useState(false);
-  let [usernameTaken, setUsernameTaken] = useState(false)
+  let [usernameTaken, setUsernameTaken] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -24,82 +31,85 @@ export default function Login() {
     })();
   }, []);
 
-  let loginSignup = <Button
-                      style={globalStyles.button}
-                      title="Sign In with Google"
-                      onPress={async () => {
-                        const _authState = await signInAsync();
-                        setAuthState(_authState);
-                        setLoading(true);
-                        // search for sub not on database
-                        let sub = await requestSub()
-                        let req = qs.stringify({
-                          uid: "1",
-                          appid: "capstone",
-                          access_token: "test_token",
-                          sign: "capstone",
-                          info: JSON.stringify({
-                            user_uid: sub
-                          }),
-                        });
-                        axios
-                          .post("https://www.splitvision.top/api/capstone/isUserExistByUID", req)
-                          .then(async function (response) {
-                            if (response.data.errno === 0) {
-                              setLoading(false);
-                            }
-                            if (response.data.errno === 4) {
-                              setCreateAccountId(await requestSub());
-                              await signOutAsync(_authState);
-                              setAuthState(null);
-                              setCreatingAccount(true);
-                              setLoading(false);
-                            }
-                          });
-                      }}
-                    />
+  let loginSignup = (
+    <Button
+      style={globalStyles.button}
+      title="Sign In with Google"
+      onPress={async () => {
+        const _authState = await signInAsync();
+        setAuthState(_authState);
+        setLoading(true);
+        // search for sub not on database
+        let sub = await requestSub();
+        let req = qs.stringify({
+          uid: "1",
+          appid: "capstone",
+          access_token: "test_token",
+          sign: "capstone",
+          info: JSON.stringify({
+            user_uid: sub,
+          }),
+        });
+        axios
+          .post(
+            "https://www.splitvision.top/api/capstone/isUserExistByUID",
+            req
+          )
+          .then(async function (response) {
+            if (response.data.errno === 0) {
+              setLoading(false);
+            }
+            if (response.data.errno === 4) {
+              setCreateAccountId(await requestSub());
+              await signOutAsync(_authState);
+              setAuthState(null);
+              setCreatingAccount(true);
+              setLoading(false);
+            }
+          });
+      }}
+    />
+  );
 
   if (authState) {
-    loginSignup = <Button
-                    style={globalStyles.button}
-                    title="Sign Out"
-                    onPress={async () => {
-                      await signOutAsync(authState);
-                      setAuthState(null);
-                    }}
-                  />
+    loginSignup = (
+      <Button
+        style={globalStyles.button}
+        title="Sign Out"
+        onPress={async () => {
+          await signOutAsync(authState);
+          setAuthState(null);
+        }}
+      />
+    );
   }
 
-  if(loading) {
+  if (loading) {
     return (
       <View style={globalStyles.container}>
         <Text style={styles.headers}>Loading</Text>
       </View>
-    )
+    );
   }
-  if(!creatingAccount) {
-    return (
-      <View style={globalStyles.container}>
-        {loginSignup}
-      </View>
-    )
+  if (!creatingAccount) {
+    return <View style={globalStyles.container}>{loginSignup}</View>;
   } else {
     let inputStyle = globalStyles.input;
-    if(usernameTaken) {
+    if (usernameTaken) {
       inputStyle = globalStyles.badInput;
     }
     return (
       <View style={globalStyles.container}>
         <Text>Username:</Text>
-        <TextInput 
-          style={inputStyle} 
-          onChangeText={text => setUsername(text)} 
-          value={username} 
+        <TextInput
+          style={inputStyle}
+          onChangeText={(text) => setUsername(text)}
+          value={username}
         />
         <Button
           style={globalStyles.button}
           title="Create Account"
-          onPress={async() => {
+          onPress={async () => {
             // check if username is taken
             setLoading(true);
             let req = qs.stringify({
@@ -108,11 +118,14 @@ export default function Login() {
               access_token: "test_token",
               sign: "capstone",
               info: JSON.stringify({
-                user_name: username
+                user_name: username,
               }),
             });
             axios
-              .post("https://www.splitvision.top/api/capstone/getUserByName", req)
+              .post(
+                "https://www.splitvision.top/api/capstone/getUserByName",
+                req
+              )
               .then(async function (response) {
                 if (response.data.errno === 0) {
                   setUsernameTaken(true);
@@ -128,7 +141,7 @@ export default function Login() {
                       user_name: username,
                       user_pwd: "na",
                       user_email: "na",
-                      user_phone: "na"
+                      user_phone: "na",
                     }),
                   });
                   axios
@@ -145,17 +158,18 @@ export default function Login() {
           }}
         />
       </View>
-    )
+    );
   }
 }
 
 let config = {
-  issuer: 'https://accounts.google.com',
-  scopes: ['email'],
-  clientId: '460355842723-bsr9a76tc6i4d2k63km9q5db5hii80bm.apps.googleusercontent.com',
+  issuer: "https://accounts.google.com",
+  scopes: ["email"],
+  clientId:
+    "460355842723-bsr9a76tc6i4d2k63km9q5db5hii80bm.apps.googleusercontent.com",
 };
 
-let StorageKey = '@RSVPme:GoogleOAuthKey';
+let StorageKey = "@RSVPme:GoogleOAuthKey";
 
 export async function signInAsync() {
   let authState = await AppAuth.authAsync(config);
@@ -170,8 +184,8 @@ async function cacheAuthAsync(authState) {
 export async function getCachedAuthAsync() {
   let value = await AsyncStorage.getItem(StorageKey);
   let authState = JSON.parse(value);
-  if(authState) {
-    if(checkIfTokenExpired(authState)) {
+  if (authState) {
+    if (checkIfTokenExpired(authState)) {
       return refreshAuthAsync(authState);
     } else {
       return authState;
@@ -198,30 +212,30 @@ export async function signOutAsync({ accessToken }) {
     });
     await AsyncStorage.removeItem(StorageKey);
     return null;
-  } catch(e) {
+  } catch (e) {
     alert(`Failed to revoke token: ${e.message}`);
   }
 }
 
 export async function requestSub() {
-  var authState = await getCachedAuthAsync()
-  if(authState) {
+  var authState = await getCachedAuthAsync();
+  if (authState) {
     var myHeaders = new Headers();
-    myHeaders.append("Authorization", `Bearer ${authState['accessToken']}`);
+    myHeaders.append("Authorization", `Bearer ${authState["accessToken"]}`);
 
     var requestOptions = {
-      method: 'GET',
+      method: "GET",
       headers: myHeaders,
-      redirect: 'follow'
+      redirect: "follow",
     };
 
-    return fetch("https://www.googleapis.com/oauth2/v3/userinfo", requestOptions).then(
-      response => response.text()
-    ).then(
-      result => "google:".concat(JSON.parse(result)["sub"])
-    ).catch(
-      error => console.log('error', error)
-    );
+    return fetch(
+      "https://www.googleapis.com/oauth2/v3/userinfo",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => "google:".concat(JSON.parse(result)["sub"]))
+      .catch((error) => console.log("error", error));
   }
   return null;
 }
@@ -229,6 +243,6 @@ export async function requestSub() {
 const styles = StyleSheet.create({
   headers: {
     textAlign: "center",
-    fontSize: 40
-  }
-})
+    fontSize: 40,
+  },
+});
